503a00ca4e6824f5fae08b030167485da607a609
SPAPppp <francesco.pappaterra06@gmail.com>
2023-05-04 19:01:14 +0200

    OIS – Prima gara 2014

---
+++ BIglietti a Milano.py
@@ -0,0 +1,18 @@
+with open('input.txt','r') as input:
+    for word in input:
+        data=word.split()
+
+trip=int(data[0])
+CarnetTikets=int(data[1])
+TiketPrice=int(data[2])
+CarnetPrice=int(data[3])
+
+
+if CarnetPrice<(trip%CarnetTikets)*TiketPrice:
+    answer=((trip//CarnetTikets)+1)*CarnetPrice
+    with open('output.txt','w') as output:
+        output.write(str(answer))
+else:
+    answer=((trip//CarnetTikets)*CarnetPrice)+((trip%CarnetTikets)*TiketPrice)
+    with open('output.txt','w') as output:
+        output.write(str(answer)) 
